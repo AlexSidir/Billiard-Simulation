c31497a79c732a14626affdfd4086a3cfdc303c4
Ducklex <alex_sid@hotmail.com>
2018-12-11 00:25:07 +0100

    .

--- billiard-simulation.js
+++ billiard-simulation.js
@@ -3,12 +3,27 @@ const renderer = new THREE.WebGLRenderer({canvas:canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor('rgb(255,255,255)');
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.y = 3;
 camera.position.z = 0.1;
+
+var controls = new THREE.OrbitControls( camera );
+camera.position.set( 0, 4, 2 );
+controls.update();
+
+const floorDefaults = {
+  length:15,
+  depth: 0.1,
+  width: 15
+}
+
+const wallDefaults = {
+  length: 4,
+  depth: 0.1,
+  width: 15
+}
 
 const tableDefaults = {
   length: 3.57,
@@ -35,41 +50,126 @@ spotLight.shadow.mapSize.width = 1024;
 spotLight.shadow.mapSize.height = 1024;
 spotLight.shadow.camera.near = 0.5;
 spotLight.shadow.camera.far = 100;
-spotLight.distance = 2000;
+spotLight.distance = 100;
+spotLight.angle = Math.PI/2 + Math.PI/4;
 spotLight.target.position.set(-1, 0, 1 );
 //spotLight.shadow.camera.fov = 30;
 //spotLight.shadowDarkness = 0.95;
 spotLight.intensity = 1.5;
 scene.add( spotLight );
 
+const txtLoader = new THREE.TextureLoader();
+
 let table;
-let floorGeometry = new THREE.BoxGeometry( 15, 0.1,15   );
-let floorMaterial = new THREE.MeshPhongMaterial( {color: 0x808080} );
+
+const floorTexture = txtLoader.load("Skins/floor.jpg");
+let floorGeometry = new THREE.BoxGeometry( floorDefaults.length, floorDefaults.depth, floorDefaults.width);
+let floorMaterial = new THREE.MeshPhongMaterial( { map:txtLoader.load("Skins/floor.jpg")});
 let floor = new THREE.Mesh( floorGeometry, floorMaterial );
-floor.castShadow = true; //default is false
+floor.castShadow = true;
 floor.receiveShadow = true;
 scene.add( floor );
 
+let ceilingGeometry = new THREE.BoxGeometry( floorDefaults.length, floorDefaults.depth, floorDefaults.width);
+let ceilingMaterial = new THREE.MeshPhongMaterial( {color: 0xA52A2A} );
+let ceiling = new THREE.Mesh( ceilingGeometry, ceilingMaterial );
+ceiling.castShadow = true;
+ceiling.receiveShadow = true;
+ceiling.position.set( 0, wallDefaults.length, 0);
+scene.add( ceiling );
+
+let wallLeftGeometry = new THREE.BoxGeometry( wallDefaults.length, wallDefaults.depth, wallDefaults.width);
+let wallLeftMaterial = new THREE.MeshBasicMaterial( {color: 0xB9BD96, map:txtLoader.load("Skins/wall.jpg")});
+let wallLeft = new THREE.Mesh( wallLeftGeometry, wallLeftMaterial );
+wallLeft.rotation.set( 0, 0, Math.PI/2);
+wallLeft.position.set( floorDefaults.length/2, wallDefaults.length/2, 0);
+scene.add( wallLeft );
+
+let wallRightGeometry = new THREE.BoxGeometry( wallDefaults.length, wallDefaults.depth, wallDefaults.width);
+let wallRightMaterial = new THREE.MeshBasicMaterial( {color: 0xB9BD96, map:txtLoader.load("Skins/wall.jpg")});
+let wallRight = new THREE.Mesh( wallRightGeometry, wallRightMaterial );
+wallRight.rotation.set( 0, 0, Math.PI/2);
+wallRight.position.set( -floorDefaults.length/2, wallDefaults.length/2, 0);
+scene.add( wallRight );
+
+let wallTopGeometry = new THREE.BoxGeometry( wallDefaults.length, wallDefaults.depth, wallDefaults.width);
+let wallTopMaterial = new THREE.MeshBasicMaterial( {color: 0xB9BD96, map:txtLoader.load("Skins/wall.jpg")});
+let wallTop = new THREE.Mesh( wallTopGeometry, wallTopMaterial );
+wallTop.rotation.set( 0, Math.PI/2, Math.PI/2);
+wallTop.position.set( 0, wallDefaults.length/2, floorDefaults.length/2);
+scene.add( wallTop );
+
+let wallBottomGeometry = new THREE.BoxGeometry( wallDefaults.length, wallDefaults.depth, wallDefaults.width);
+let wallBottomMaterial = new THREE.MeshBasicMaterial( {color: 0xB9BD96, map:txtLoader.load("Skins/wall.jpg")});
+let wallBottom = new THREE.Mesh( wallBottomGeometry, wallBottomMaterial );
+wallBottom.rotation.set( 0, Math.PI/2, Math.PI/2);
+wallBottom.position.set( 0, wallDefaults.length/2, -floorDefaults.length/2);
+scene.add( wallBottom );
+
+//---------------------------------------------------------------------------------------------------------
+var platforms = [],
+platformSize = 20,
+platformWidth = 3.58/2 - 0.2,
+platformHeight = 1.57/2 - 0.2;
+
+function generatePlatforms(k) {
+  var placed = 0,
+  maxAttempts = k*10;
+  while(placed < k && maxAttempts > 0) {
+    var x = Math.random()*platformWidth,
+    y = Math.random()*platformHeight,
+    available = true;
+    //console.log(platforms);
+    for(let i=0; i<platforms.length; i++) {//for(var point in platforms) {
+      console.log("x + " + Math.abs(platforms[i].y +y));
+      console.log("y + " + Math.abs(platforms[i].x +x));
+      console.log("x random + " + y);
+      console.log("y random + " + x );
+      if(Math.abs(platforms[i].x - x) < platformSize && Math.abs(platforms[i].y - y) < platformSize) {
+        available = false;
+        break;
+      }
+    }
+    if(available) {
+      x = (x) * (Math.random() >= 0.5 ? -1 : 1 );
+      y = (y) * (Math.random() >= 0.5 ? -1 : 1 );
+      platforms.push({
+        x: x,
+        y: y
+      });
+      placed += 1;
+    }
+    maxAttempts -= 1;
+  }
+
+  for(let j = 0; j < platforms.length; j++) {
+    const txtLoader = new THREE.TextureLoader();
+    var geometryBall8 = new THREE.SphereGeometry( ballDefaults.radius, ballDefaults.widthSegments, ballDefaults.heightSegments );
+    let str = "PoolBallSkins/Ball"+ (j +8) +".jpg";
+    const ball8Texture = txtLoader.load(str);
+    var materialBall8 = new THREE.MeshBasicMaterial( {color: 0xffffff, map:ball8Texture} );
+    var ball8 = new THREE.Mesh( geometryBall8, materialBall8 );
+    ball8.position.set( platforms[j].x,tableDefaults.yPosition + tableDefaults.depth/2 + ballDefaults.radius, platforms[j].y);
+    ball8.castShadow = true;
+    ball8.receiveShadow = true;
+    scene.add( ball8 );
+  }
+}
+
+//generatePlatforms(8);
+//console.log(platforms);
 createTable();
 
-let sphereGeometry = new THREE.SphereGeometry( 0.3, 16, 16 );
-let material1 = new THREE.MeshBasicMaterial( {
-  color: 0xFFFF00,
-  //  polygonOffset: true,
-  //  polygonOffsetFactor: 1, // positive value pushes polygon further away
-  //    polygonOffsetUnits: 1
-} );
+generatePlatforms(8);
+//---------------------------------------------------------------------------------------------------------
 
-let mesh = new THREE.Mesh( sphereGeometry, material1 );
-scene.add( mesh )
-mesh.position.y = 3.5;
+var geometryBall8 = new THREE.SphereGeometry( 0.3, 16, 16);
+var materialBall8 = new THREE.MeshBasicMaterial( {color: 0xffffff, map: txtLoader.load("Skins/lamp.jpg")} );
+var  lamp = new THREE.Mesh( geometryBall8, materialBall8 );
+lamp.position.y = 3.5;
+scene.add( lamp );
 
-let geo = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
-let mat = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2 } );
-let wireframe = new THREE.LineSegments( geo, mat );
-mesh.add( wireframe );
-
-let wireLampGeometry = new THREE.BoxGeometry( 0.01, 1, 0.01   );
+let wireLampGeometry = new THREE.BoxGeometry( 0.01, 0.5, 0.01   );
 let wireLampMaterial = new THREE.MeshPhongMaterial( {color: 0x808080} );
 let wireLamp = new THREE.Mesh( wireLampGeometry, wireLampMaterial );
 wireLamp.position.y = 4;
@@ -81,24 +181,24 @@ function createTable () {
   let tableMaterial = new THREE.MeshLambertMaterial( {color: 0x25eb78} );
   table = new THREE.Mesh( tableGeometry, tableMaterial );
   table.position.y = tableDefaults.yPosition;
-  table.castShadow = true; //default is false
-  table.receiveShadow = true;
+  table.castShadow = true;
+  //table.receiveShadow = true;
   scene.add( table );
 
   let bottomSideGeometry = new THREE.BoxGeometry( tableDefaults.sideWidth  + tableDefaults.widthOffset, tableDefaults.sideLength + tableDefaults.lengthOffset, tableDefaults.sideDepth );
   let bottomSideMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let bottomSide = new THREE.Mesh( bottomSideGeometry, bottomSideMaterial );
   bottomSide.position.set(0,tableDefaults.yPosition , tableDefaults.width/2 + tableDefaults.sideDepth/2);
-  bottomSide.castShadow = true; //default is false
-  bottomSide.receiveShadow = true;
+  bottomSide.castShadow = true;
+  //bottomSide.receiveShadow = true;
   scene.add( bottomSide );
 
   let topSideGeometry = new THREE.BoxGeometry( tableDefaults.sideWidth  + tableDefaults.widthOffset, tableDefaults.sideLength + tableDefaults.lengthOffset, tableDefaults.sideDepth );
   let topSideMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let topSide = new THREE.Mesh( topSideGeometry, topSideMaterial );
   topSide.position.set(0, tableDefaults.yPosition , - tableDefaults.width/2 - tableDefaults.sideDepth/2);
-  topSide.castShadow = true; //default is false
-  topSide.receiveShadow = true;
+  topSide.castShadow = true;
+  //  topSide.receiveShadow = true;
   scene.add( topSide );
 
   let rightSideGeometry = new THREE.BoxGeometry( tableDefaults.width, tableDefaults.sideLength + tableDefaults.lengthOffset, tableDefaults.sideDepth );
@@ -106,8 +206,8 @@ function createTable () {
   let rightSide = new THREE.Mesh( rightSideGeometry, rightSideMaterial );
   rightSide.position.set( - tableDefaults.length/2 - tableDefaults.sideDepth/2, tableDefaults.yPosition, 0);
   rightSide.rotation.y = Math.PI/2;
-  rightSide.castShadow = true; //default is false
-  rightSide.receiveShadow = true;
+  rightSide.castShadow = true;
+  //  rightSide.receiveShadow = true;
   scene.add( rightSide );
 
   let leftSideGeometry = new THREE.BoxGeometry( tableDefaults.width, tableDefaults.sideLength + tableDefaults.lengthOffset, tableDefaults.sideDepth );
@@ -115,54 +215,54 @@ function createTable () {
   let leftSide = new THREE.Mesh( leftSideGeometry, leftSideMaterial );
   leftSide.position.set( tableDefaults.length/2 + tableDefaults.sideDepth/2, tableDefaults.yPosition, 0);
   leftSide.rotation.y = Math.PI/2;
-  leftSide.castShadow = true; //default is false
-  leftSide.receiveShadow = true;
+  leftSide.castShadow = true;
+  //  leftSide.receiveShadow = true;
   scene.add( leftSide );
 
   let leftTopLegGeometry = new THREE.BoxGeometry( 0.1, tableDefaults.yPosition, 0.1   );
   let leftTopLegMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let leftTopLeg = new THREE.Mesh( leftTopLegGeometry, leftTopLegMaterial );
   leftTopLeg.position.set(- tableDefaults.length/2, tableDefaults.yPosition/2 , -tableDefaults.width/2);
-  leftTopLeg.castShadow = true; //default is false
-  leftTopLeg.receiveShadow = true;
+  leftTopLeg.castShadow = true;
+  //  leftTopLeg.receiveShadow = true;
   scene.add( leftTopLeg );
 
   let leftBottomLegGeometry = new THREE.BoxGeometry( 0.1, tableDefaults.yPosition, 0.1   );
   let leftBottomLegMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let leftBottomLeg= new THREE.Mesh( leftBottomLegGeometry, leftBottomLegMaterial );
   leftBottomLeg.position.set(- tableDefaults.length/2,tableDefaults.yPosition/2 , tableDefaults.width/2);
-  leftBottomLeg.castShadow = true; //default is false
-  leftBottomLeg.receiveShadow = true;
+  leftBottomLeg.castShadow = true;
+  //  leftBottomLeg.receiveShadow = true;
   scene.add( leftBottomLeg );
 
   let rightTopLegGeometry = new THREE.BoxGeometry( 0.1, tableDefaults.yPosition, 0.1   );
   let rightTopLegMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let rightTopLeg = new THREE.Mesh( rightTopLegGeometry, rightTopLegMaterial );
   rightTopLeg.position.set( tableDefaults.length/2,tableDefaults.yPosition/2 , -tableDefaults.width/2);
-  rightTopLeg.castShadow = true; //default is false
-  rightTopLeg.receiveShadow = true;
+  rightTopLeg.castShadow = true;
+  //  rightTopLeg.receiveShadow = true;
   scene.add( rightTopLeg );
 
   let rightBottomLegGeometry = new THREE.BoxGeometry( 0.1, tableDefaults.yPosition, 0.1   );
   let rightBottomLegMaterial = new THREE.MeshStandardMaterial( {color: 0xD2691E} );
   let rightBottomLeg = new THREE.Mesh( rightBottomLegGeometry, rightBottomLegMaterial );
   rightBottomLeg.position.set( tableDefaults.length/2,tableDefaults.yPosition/2 , tableDefaults.width/2);
-  rightBottomLeg.castShadow = true; //default is false
-  rightBottomLeg.receiveShadow = true;
+  rightBottomLeg.castShadow = true;
+  //  rightBottomLeg.receiveShadow = true;
   scene.add( rightBottomLeg );
 }
 balls();
 
-
 function balls() {
-
-  const txtLoader = new THREE.TextureLoader();
+  /*//const txtLoader = new THREE.TextureLoader();
 
   var geometryBall8 = new THREE.SphereGeometry( ballDefaults.radius, ballDefaults.widthSegments, ballDefaults.heightSegments );
   const ball8Texture = txtLoader.load("PoolBallSkins/Ball8.jpg");
   var materialBall8 = new THREE.MeshBasicMaterial( {color: 0xffffff, map:ball8Texture} );
   var ball8 = new THREE.Mesh( geometryBall8, materialBall8 );
   ball8.position.set( 0,tableDefaults.yPosition + tableDefaults.depth/2 + ballDefaults.radius, 0);
+  ball8.castShadow = true;
+  ball8.receiveShadow = true;
   scene.add( ball8 );
 
   var geometryBall9 = new THREE.SphereGeometry( ballDefaults.radius, ballDefaults.widthSegments, ballDefaults.heightSegments );
@@ -170,6 +270,8 @@ function balls() {
   var materialBall9 = new THREE.MeshBasicMaterial( {color: 0xffffff, map:ball9Texture} );
   var ball9 = new THREE.Mesh( geometryBall9, materialBall9 );
   ball9.position.set( 0.1,tableDefaults.yPosition + tableDefaults.depth/2 + ballDefaults.radius, 0);
+  ball9.castShadow = true;
+  ball9.receiveShadow = true;
   scene.add( ball9 );
 
   var geometryBall10 = new THREE.SphereGeometry( ballDefaults.radius, ballDefaults.widthSegments, ballDefaults.heightSegments );
@@ -212,10 +314,10 @@ function balls() {
   var materialBall15 = new THREE.MeshBasicMaterial( {color: 0xffffff, map:ball15Texture} );
   var ball15 = new THREE.Mesh( geometryBall15, materialBall15 );
   ball15.position.set(- 0.2,tableDefaults.yPosition + tableDefaults.depth/2 + ballDefaults.radius, 0);
-  scene.add( ball15 );
+  scene.add( ball15 );*/
 }
-const controls = new THREE.TrackballControls( camera, canvas );
-controls.rotateSpeed = 2;
+//const controls = new THREE.TrackballControls( camera, canvas );
+//controls.rotateSpeed = 2;
 
 function render() {
   requestAnimationFrame(render);
